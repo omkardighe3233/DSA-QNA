3687f31b3c0356c9040efb3a010cdfcbb0466bc6
Omkar Dighe <omkardighe3233@gmail.com>
2026-07-14 14:03:40 +0530

    ✅ Container With Most Water | Medium | Java | Runtime: 5 ms (82.64%) | Memory: 77.3 MB | 2026-07-14

--- medium/0011-container-with-most-water/solution.java
+++ medium/0011-container-with-most-water/solution.java
@@ -1,22 +1,22 @@
-class Solution {
-    public int maxArea(int[] height) 
-    {
-       int lp = 0;
-       int rp = height.length-1;
-       int currwater=0;
-        while(lp<rp)
-        {
-            int hig = Math.min(height[lp],height[rp]);
-            int wid = rp-lp;
-            currwater = Math.max( wid*hig , currwater );
-            if(height[lp] < height[rp])
-            {
-                lp++;
-            }
-            else{
-                rp--;
-            }
-        }
-       return currwater; 
-    }
+class Solution {
+    public int maxArea(int[] height) 
+    {
+       int lp = 0;
+       int rp = height.length-1;
+       int currwater=0;
+        while(lp<rp)
+        {
+            int hig = Math.min(height[lp],height[rp]);
+            int wid = rp-lp;
+            currwater = Math.max( wid*hig , currwater );
+            if(height[lp] < height[rp])
+            {
+                lp++;
+            }
+            else{
+                rp--;
+            }
+        }
+       return currwater; 
+    }
 }
