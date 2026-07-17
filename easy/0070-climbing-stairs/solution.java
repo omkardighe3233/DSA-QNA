0ed89a57122f336edacd0ff0d35b9718d077ed80
Omkar Dighe <omkardighe3233@gmail.com>
2026-07-17 17:17:18 +0530

    ✅ Climbing Stairs | Easy | Java | Runtime: 0 ms (100.00%) | Memory: 42.1 MB | 2026-07-17

--- easy/0070-climbing-stairs/solution.java
+++ easy/0070-climbing-stairs/solution.java
@@ -1,10 +1,21 @@
 class Solution {
-    public int climbStairs(int n) 
-    {
-        if(n==1 || n==2) return n;
-        //int ans = 1;
-        
-        return climbStairs(n-1) + climbStairs(n-2);
-        
+    public int climbStairs(int n) {
+
+        if (n <= 2)
+            return n;
+
+        ArrayList<Integer> kk = new ArrayList<>();
+
+        kk.add(1);
+        kk.add(2);
+
+        while (kk.size() < n) {
+            int last = kk.get(kk.size() - 1);
+            int secondLast = kk.get(kk.size() - 2);
+
+            kk.add(last + secondLast);
+        }
+
+        return kk.get(kk.size() - 1);
     }
 }
