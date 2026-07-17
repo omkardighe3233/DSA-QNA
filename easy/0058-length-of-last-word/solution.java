6044a240dbd5affe9ac99bd3b4682a14ab41ae16
Omkar Dighe <omkardighe3233@gmail.com>
2026-07-17 16:10:59 +0530

    ✅ Length of Last Word | Easy | Java | Runtime: 0 ms | Memory: 42.5 MB | 2026-07-17

---
+++ easy/0058-length-of-last-word/solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int lengthOfLastWord(String s) 
+    {
+       
+       s = s.trim();
+       int i = s.length()-1;
+       int count = 0;
+       char ch = ' ';
+      while(s.charAt(i) != ch)
+      {
+            i--;
+            count++;
+      }
+      return count;
+    }
+
+}
