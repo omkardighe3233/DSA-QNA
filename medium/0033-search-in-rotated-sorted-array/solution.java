ec48597ae2632245b5bec8a88c44a729829e3d05
Omkar Dighe <omkardighe3233@gmail.com>
2026-07-22 23:21:44 +0530

    ✅ Search in Rotated Sorted Array | Medium | Java | Runtime: 0 ms | Memory: 42.6 MB | 2026-07-22

--- medium/0033-search-in-rotated-sorted-array/solution.java
+++ medium/0033-search-in-rotated-sorted-array/solution.java
@@ -1,20 +1,20 @@
-class Solution {
-    public int search(int[] nums, int target) 
-    {
-        int n= nums.length;
-     
-        {
-            n--;
-        }else if(nums[n/2] ==target){
-        return -1;
-            n++;
-        }else{
-            n= (n/2)+1;
-        }
-    }
-    }
-     while(n>=0 || n<nums.length){
-           if( nums[n/2] < target)
+class Solution {
+    public int search(int[] nums, int target) 
+    {
+        int n= nums.length-1;
+     
+     while(n>=0 || n<nums.length){
+           if( nums[n/2] < target)
+        {
+            n--;
+        }else if(nums[n/2] ==target){
+            n++;
+        }else{
 
-            return n;
+            n= (n/2)+1;
+            return n;
+        }
+    }
+        return -1;
+    }
 }
