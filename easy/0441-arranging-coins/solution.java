22a7ea38de2fdb9af11cb1ca22cdf7362decd9e6
Omkar Dighe <omkardighe3233@gmail.com>
2026-07-31 20:24:58 +0530

    ✅ Arranging Coins | Easy | Java | Runtime: 0 ms | Memory: 42.2 MB | 2026-07-31

---
+++ easy/0441-arranging-coins/solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int arrangeCoins(int n) 
+    {
+        for(int i = 1; i<=n ; i++)
+        {
+            count = count + i;
+        }
+        return 0;
+        int count = 0;
+            
+            if(count>n)
+            {
+                return i;
+            }
+    }
+                i = i-1;
+}
